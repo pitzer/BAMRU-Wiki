edf1c8a07da42313a8e38c9d1aa65cce6a3d9c07
andyl <andy@r210.com>
2012-06-02 16:02:52 -0700

    updated deploy scripts

--- Capfile
+++ Capfile
@@ -20,10 +20,9 @@ Dir.glob("config/deploy/shared/base/*.rb").each {|f| require base_dir + '/' + f}
 Dir.glob("config/deploy/shared/recipes/*.rb").each {|f| require base_dir + '/' + f}
 
 # ===== Package Definitions =====
-require base_dir + "/config/deploy/shared/packages/nginx"
+# require base_dir + "/config/deploy/shared/packages/sqlite"
+require base_dir + "/config/deploy/shared/packages/mediawiki"
 require base_dir + "/config/deploy/shared/packages/foreman"
-require base_dir + "/config/deploy/shared/packages/sqlite"
-require base_dir + "/config/deploy/shared/packages/postgresql"
 
 # ===== App-Specific Tasks =====
 
--- config/deploy/shared/packages/mediawiki.rb
+++ config/deploy/shared/packages/mediawiki.rb
@@ -1,21 +1,14 @@
-after "deploy:setup", "mediawiki:setup"
+Capistrano::Configuration.instance(:must_exist).load do
 
-namespace :mediawiki do
-  desc "Setup mediawiki initializer and app configuration"
-  task :setup, roles: :app do
-    run "mkdir -p #{shared_path}/config"
-    template "mediawiki.rb.erb", mediawiki_config
-    template "mediawiki_init.erb", "/tmp/mediawiki_init"
-    run "chmod +x /tmp/mediawiki_init"
-    run "#{sudo} mv /tmp/mediawiki_init /etc/init.d/mediawiki_#{application}"
-    run "#{sudo} update-rc.d -f mediawiki_#{application} defaults"
-  end
+  after "deploy:update", "mediawiki:nginx_conf"
 
-  %w[start stop restart].each do |command|
-    desc "#{command} mediawiki"
-    task command, roles: :app do
-      run "service mediawiki_#{application} #{command}"
+  namespace :mediawiki do
+
+    desc "Export an mediawiki config file."
+    task :nginx_conf, :role => :web do
+      template "nginx_mediawiki.erb", "/etc/nginx/conf.d/#{app_name}.conf"
     end
-    after "deploy:#{command}", "mediawiki:#{command}"
+
   end
+
 end
